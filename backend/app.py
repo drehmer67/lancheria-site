a27b2e5cc0cfd26c2e522fb170dbf4c0296d4c2f
Diablo Drehmer <yagorsbarber@gmail.com>
2026-04-15 14:09:14 -0300

    backend lancheria

--- backend/app.py
+++ backend/app.py
@@ -64,4 +64,4 @@ def deletar_pedido(index):
 
 # ================= RODAR =================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000)
